148ca22e6c56b977dc9232e5e20773e1fa5588c4
Rupert <rupert@datalinktech.com.au>
2016-09-23 13:36:31 +1000

    move functions to bash/functions

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,4 +1,4 @@
-ln -s ~/.dotfiles/ubuntu/bash ~/.bash
+ln -s ~/.dotfiles/ubuntu ~/.bash
 mv ~/.bashrc ~/.bashrc.old
 ln -s ~/.dotfiles/ubuntu/bashrc ~/.bashrc
 ln -s ~/.dotfiles/ubuntu/localrc ~/.localrc

487d1acd8d7a7d8ae450f575f47aa3a6c38cccf8
Rupert <rupert@datalinktech.com.au>
2017-04-26 08:37:36 +1000

    Add dips-deploy + database panes to tmux-dips.sh

--- tmux-scripts/tmux-dips.sh
+++ tmux-scripts/tmux-dips.sh
@@ -1,21 +1,27 @@
 #!/bin/zsh
 tmux new-session -s dips -n "dips-client" -d
-tmux send-keys -t dips:1 "cd /Projects/dips/dips-client" Enter "vim" Enter ":NERDTree" Enter
+tmux send-keys -t dips:1 "cd /Projects/dips/dips-client" Enter 
 
 tmux new-window -t dips:2 -n "npm start"
 tmux send-keys -t dips:2 "cd /Projects/dips/dips-client" Enter "npm start"
 
 tmux new-window -t dips:3 -n "dips-api"
-tmux send-keys -t dips:3 "cd /Projects/dips/dips-api" Enter "vim" Enter ":NERDTree" Enter
+tmux send-keys -t dips:3 "cd /Projects/dips/dips-api" Enter
 
 tmux new-window -t dips:4 -n "rails-server"
 tmux send-keys -t dips:4 "cd /Projects/dips/dips-api" Enter "rails s -b 0.0.0.0 -p 3003" Enter
 
 tmux new-window -t dips:5 -n "dips-listener"
-tmux send-keys -t dips:5 "cd /Projects/dips/dips-listener" Enter "vim" Enter ":NERDTree" Enter
+tmux send-keys -t dips:5 "cd /Projects/dips/dips-listener" Enter
 
 tmux new-window -t dips:6 -n "listener"
 tmux send-keys -t dips:6 "cd /Projects/dips/dips-listener" Enter
+
+tmux new-window -t dips:7 -n "dips-deploy"
+tmux send-keys -t dips:7 "cd /Projects/dips/dips-deploy" Enter
+
+tmux new-window -t dips:8 -n "database"
+tmux send-keys -t dips:8 "cd /Projects/dips/dips-api" Enter "psql -h 127.0.0.1 -p 5432 -U dips atlas_development"
 
 tmux select-window -t dips:1
 

8b1b15d40e881ea0d4166ab93e47fe20fe86bdcb
Rupert <rupert@datalinktech.com.au>
2017-05-11 16:18:54 +1000

    Update tmux-iris-server to use vagrant

--- tmux-scripts/tmux-iris-server.sh
+++ tmux-scripts/tmux-iris-server.sh
@@ -13,20 +13,20 @@ tmux split-window -v -p 50
 # NPM Start
 tmux select-pane -t 2
 sleep .3
-tmux send-keys "npm start" Enter
+tmux send-keys "cd /Projects/rails/iris/" Enter "npm start"
 
 # Rails Server
 tmux select-pane -t 3
 sleep .3
-tmux send-keys "sshdev rupert" Enter "ji" Enter "sleep 5" Enter "zserver" Enter
+tmux send-keys "cd /Projects/dboxes" Enter "vagrant ssh" Enter "cd /var/www/rails/iris" Enter "sleep 5" Enter "zserver" Enter
 
 # ZStart Pane
 tmux select-pane -t 1
 sleep .3
-tmux send-keys "sshdev rupert" Enter "ji" Enter "zstart" Enter
+tmux send-keys "cd /Projects/dboxes" Enter "vagrant ssh" Enter "cd /var/www/rails/iris" Enter "zstart" Enter
 
 tmux new-window -t iris:2 -n "zeus test spec"
-tmux send-keys -t iris:2 "sshdev rupert" Enter "ji" Enter "sleep 5" Enter "zeus test spec"
+tmux send-keys -t iris:2 "cd /Projects/dboxes" Enter "vagrant ssh" Enter "cd /var/www/rails/iris" Enter "sleep 5" Enter "zeus test spec"
 
 tmux new-window -t iris:3 -n "database"
 tmux send-keys -t iris:3 "psql -h 127.0.0.1 -p 5432 -U dbadmin datalink_development"

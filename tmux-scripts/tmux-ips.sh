6b5b22ede49745ad1764b42e133e2486c17b2c74
Rupert <rupert@datalinktech.com.au>
2017-06-05 10:01:06 +1000

    Update devices

--- tmux-scripts/tmux-ips.sh
+++ tmux-scripts/tmux-ips.sh
@@ -2,29 +2,29 @@
 tmux new-session -s devices -n "devices" -d
 
 tmux split-window -h -p 50
-tmux send-keys "ping 10.0.0.14" Enter
+tmux send-keys "ping 10.0.0.13" Enter
 tmux split-window -v -p 80
-tmux send-keys "ping 10.0.0.20" Enter
+tmux send-keys "ping 10.0.0.30" Enter
 tmux split-window -v -p 80
-tmux send-keys "ping 10.0.0.21" Enter
+tmux send-keys "ping 10.0.0.31" Enter
 tmux split-window -v -p 80
-tmux send-keys "ping 10.0.0.22" Enter
+tmux send-keys "ping 10.0.0.33" Enter
 tmux split-window -v -p 70
-tmux send-keys "ping 10.0.0.23" Enter
+tmux send-keys "ping 10.0.0.33" Enter
 tmux split-window -v 
-tmux send-keys "ping 10.0.0.24" Enter
+tmux send-keys "ping 10.0.0.34" Enter
 
 tmux select-pane -t 1
 tmux split-window -v -p 80
-tmux send-keys "ping 10.0.0.25" Enter
+tmux send-keys "ping 10.0.0.35" Enter
 tmux split-window -v -p 80
-tmux send-keys "ping 10.0.0.26" Enter
+tmux send-keys "ping 10.0.0.36" Enter
 tmux split-window -v -p 80
-tmux send-keys "ping 10.0.0.27" Enter
+tmux send-keys "ping 10.0.0.37" Enter
 tmux split-window -v -p 70
-tmux send-keys "ping 10.0.0.28" Enter
+tmux send-keys "ping 10.0.0.38" Enter
 tmux split-window -v 
-tmux send-keys "ping 10.0.0.29" Enter
+tmux send-keys "ping 10.0.0.39" Enter
 
 tmux select-window -t devices:1
 
